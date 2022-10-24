a9d2e152cc73dd1085f3a9a83b783c6a92d91cd6
AnuragShahare <112797509+AnuragShahare@users.noreply.github.com>
2022-10-24 15:48:34 -0700

    Add files via upload

---
+++ M4A3/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+  
+
+   // const mongoString = process.env.DATABASE.replace('jenil2704', process.env.DB_PASSWORD);
+
+
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
